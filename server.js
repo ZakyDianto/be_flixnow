724a12517e68052be9056f7b52b57472ddf3c861
ZakyDianto <140376583+ZakyDianto@users.noreply.github.com>
2025-05-28 20:34:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,17 +6,20 @@ const path = require(`path`)
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true })); // untuk menerima form body (x-www-form-urlencoded)
 
 const userRoute = require(`./routes/user.route`)
 const adminRoute = require(`./routes/admin.routes`)
 const filmRoute = require(`./routes/film.routes`)
 const orderRoute = require(`./routes/order.routes`)
+const authRoute = require(`./routes/auth.routes`)
 
 app.use(`/user`, userRoute) 
 app.use(`/admin`, adminRoute) 
 app.use(`/film`, filmRoute) 
 app.use(`/order`, orderRoute) 
+app.use(`/auth`, authRoute) 
 
 
 app.listen(PORT, () => {
